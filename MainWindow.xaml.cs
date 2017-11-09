b232257d2c5c700541e9828d0b849358478852a4
smartinezl <33464782+smartinezl@users.noreply.github.com>
2017-11-09 18:47:38 +0100

    Pujada d'una aplicació

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,219 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Signature
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static string imatge;
+
+        static string signatura;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLoadImg_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Selecciona una imatge";
+            op.Filter = "Tots els arxius d'imatge|*.jpg;*.jpeg;*.png|" +
+                        "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+                        "PNG (*.png)|*.png|" +
+                        "Tots els arxius|*.*";
+            if (op.ShowDialog() == true)
+            {
+                imgFoto.Source = new BitmapImage(new Uri(op.FileName));
+                imatge = op.FileName;
+            }
+        }
+
+        private void BtnLoadSignature_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Selecciona la signatura";
+            op.Filter = "RSA|*.rsa|" +
+                        "Tots els arxius|*.*";
+            if (op.ShowDialog() == true)
+            {
+                txtSignature.Text = File.ReadAllText(op.FileName);
+                signatura = op.FileName;
+            }
+        }
+
+        private void BtnSign_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Signa.KeysArePresent())
+            {
+                MessageBoxKeyNotFound();
+            }
+            else
+            {
+                try
+                {
+                    FileStream fsImatge = new FileStream(imatge, FileMode.Open, FileAccess.Read);
+                    var dialog = new System.Windows.Forms.FolderBrowserDialog();
+                    dialog.Description = "Selecciona la carpeta on es desarà la signatura.";
+                    System.Windows.Forms.DialogResult res = dialog.ShowDialog();
+                    if (res == System.Windows.Forms.DialogResult.OK)
+                    {
+                        string path = dialog.SelectedPath;
+                        Signa.Sign(fsImatge, path);
+                        string messageBoxText = "Imatge signada correctament.";
+                        string caption = "Signatura completada";
+                        MessageBoxButton button = MessageBoxButton.OK;
+                        MessageBoxImage icon = MessageBoxImage.None;
+                        MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
+                    }
+                    fsImatge.Close();
+                }
+                catch (FileNotFoundException)
+                {
+                    MessageBoxImageNotFound();
+                }
+                catch (ArgumentNullException)
+                {
+                    MessageBoxImageNotFound();
+                }
+            }
+        }
+
+        private void BtnCheck_Click(object sender, RoutedEventArgs e)
+        {
+            if (Signa.KeysArePresent())
+            {
+                try
+                {
+                    FileStream fsImatge = new FileStream(imatge, FileMode.Open, FileAccess.Read);
+                    FileStream fsSignatura = new FileStream(signatura, FileMode.Open, FileAccess.Read);
+                    string messageBoxText, caption;
+                    if (Signa.ValidateSignature(fsImatge, fsSignatura))
+                    {
+                        messageBoxText = "La signatura és correcta.";
+                        caption = "Signatura Correcta";
+                    }
+                    else
+                    {
+                        messageBoxText = "La signatura no és correcta.";
+                        caption = "Signatura Incorrecta";
+                    }
+                    MessageBoxButton button = MessageBoxButton.OK;
+                    MessageBoxImage icon = MessageBoxImage.None;
+                    MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
+                }
+                catch (FileNotFoundException)
+                {
+                    if (!File.Exists(imatge))
+                    {
+                        MessageBoxImageNotFound();
+                    }
+                    if (!File.Exists(signatura))
+                    {
+                        MessageBoxSignatureNotFound();
+                    }
+                }
+                catch (ArgumentNullException)
+                {
+                    if (!File.Exists(imatge))
+                    {
+                        MessageBoxImageNotFound();
+                    }
+                    if (!File.Exists(signatura))
+                    {
+                        MessageBoxSignatureNotFound();
+                    }
+                }
+            }
+            else
+            {
+                MessageBoxKeyNotFound();
+            }
+        }
+
+        private void MessageBoxImageNotFound()
+        {
+            string messageBoxText = "No s'ha entrat cap imatge. Vols triar-ne una?";
+            string caption = "Imatge no trobada";
+            MessageBoxButton button = MessageBoxButton.YesNo;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    BtnLoadImg_Click(new object(), new RoutedEventArgs());
+                    break;
+                case MessageBoxResult.No:
+                    break;
+            }
+        }
+
+        private void MessageBoxSignatureNotFound()
+        {
+            string messageBoxText = "No s'ha entrat cap signatura. Vols triar-ne una?";
+            string caption = "Signatura no trobada";
+            MessageBoxButton button = MessageBoxButton.YesNo;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    BtnLoadSignature_Click(new object(), new RoutedEventArgs());
+                    break;
+                case MessageBoxResult.No:
+                    break;
+            }
+        }
+
+        private void MessageBoxKeyNotFound()
+        {
+            string messageBoxText = "No s'han trobat les claus. Clica 'Sí' si les vols generar o 'No' si vols especificar la seva ubicació";
+            string caption = "Clau no trobada";
+            MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult res;
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    dialog.Description = "Selecciona la carpeta on es desaran les claus.";
+                    res = dialog.ShowDialog();
+                    if (res == System.Windows.Forms.DialogResult.OK)
+                    {
+                        Signa.keysPath = dialog.SelectedPath;
+                        Signa.GenerateKeys();
+                        MessageBoxKeys("generades");
+                    }
+                    break;
+                case MessageBoxResult.No:
+                    dialog.Description = "Selecciona la carpeta on es troben les claus.";
+                    res = dialog.ShowDialog();
+                    if (res == System.Windows.Forms.DialogResult.OK)
+                    {
+                        Signa.keysPath = dialog.SelectedPath;
+                        Signa.GenerateKeys();
+                        MessageBoxKeys("trobades");
+                    }
+                    break;
+                case MessageBoxResult.Cancel:
+                    break;
+            }
+        }
+
+        private void MessageBoxKeys(string msg)
+        {
+            string messageBoxText = "Claus " + msg + " correctament.";
+            string caption = "Claus " + msg;
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.None;
+            MessageBoxResult result = MessageBox.Show(messageBoxText, caption, button, icon);
+        }
+    }
+}
---
+++ Signa.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Signature
+{
+    class Signa
+    {
+        // String per conservar la ubicació de les claus.
+        public static string keysPath;
+
+        // String per conservar el nom del fitxer de la clau publica.
+        private static string publicKey = "/public.xml";
+
+        // String per conservar el nom del fitxer de la clau privada.
+        private  static string privateKey = "/private.xml";
+
+        /// <summary>
+        ///     Genera la clau que conté el parell de clau privada i pública utilitzant 1024 bytes. Guarda el conjunt de claus en els fitxers private.xml i public.xml.
+        /// </summary>
+        public static void GenerateKeys()
+        {
+            // Variables d'aces a fitxers.
+            FileStream fs = null;
+            StreamWriter sw = null;
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(1024);
+            try
+            {
+                // Guarda la clau privada
+                fs = new FileStream(keysPath + privateKey, FileMode.Create, FileAccess.Write);
+                sw = new StreamWriter(fs);
+                sw.Write(rsa.ToXmlString(true));
+                sw.Flush();
+            }
+            finally
+            {
+                if (sw != null) sw.Close();
+                if (fs != null) fs.Close();
+            }
+
+            try
+            {
+                // Guarda la clau publica
+                fs = new FileStream(keysPath + publicKey, FileMode.Create, FileAccess.Write);
+                sw = new StreamWriter(fs);
+                sw.Write(rsa.ToXmlString(false));
+                sw.Flush();
+            }
+            finally
+            {
+                if (sw != null) sw.Close();
+                if (fs != null) fs.Close();
+            }
+            rsa.Clear();
+        }
+
+        /// <summary>
+        ///     Comprova si les claus publica i privada han estat creades.
+        /// </summary>
+        /// <returns>Boolea indicant si han estat creades.</returns>
+        public static bool KeysArePresent()
+        {
+            return File.Exists(keysPath + publicKey) & File.Exists(keysPath + privateKey);
+        }
+
+        /// <summary>
+        ///     Signa el fitxer utilitzant la clau privada.
+        /// </summary>
+        /// <param name="fs">Fitxer a signar.</param>
+        public static void Sign(FileStream fs, String path)
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(1024))
+            {
+                rsa.FromXmlString(File.ReadAllText(keysPath + privateKey));
+                string finalName = path + "/" + Path.GetFileNameWithoutExtension(fs.Name) + ".rsa";
+                byte[] data = Encoding.UTF8.GetBytes(File.ReadAllText(fs.Name));
+                byte[] signature = rsa.SignData(data, new SHA1CryptoServiceProvider());
+                using (StreamWriter sw = new StreamWriter(new FileStream(finalName, FileMode.Create, FileAccess.Write)))
+                {
+                    sw.Write(Convert.ToBase64String(signature));
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Comprova si la signatura es valida per al fitxer.
+        /// </summary>
+        /// <param name="fitxer">Fitxer a comprovar.</param>
+        /// <param name="signatura">Signatura a comprovar.</param>
+        /// <returns>Cert si la signatura és vàlida i fals si no ho és.</returns>
+        public static bool ValidateSignature(FileStream fitxer, FileStream signatura)
+        {
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(1024))
+            {
+                rsa.FromXmlString(File.ReadAllText(keysPath + publicKey));
+                byte[] data = Encoding.UTF8.GetBytes(File.ReadAllText(fitxer.Name));
+                byte[] sign = Convert.FromBase64String(File.ReadAllText(signatura.Name));
+                rsa.VerifyData(data, CryptoConfig.MapNameToOID("SHA1"), sign);
+                SHA1Managed hash = new SHA1Managed();
+                byte[] hashedData = hash.ComputeHash(data);
+                return rsa.VerifyHash(hashedData, CryptoConfig.MapNameToOID("SHA1"), sign);
+            }
+        }
+    }
+}
